d1a4f8a7d506942d1ebb7a2d8aa069c6bfb356a0
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-05-24 00:26:07 +0200

    cli: Added option to specify author

--- bibcite/cli.py
+++ bibcite/cli.py
@@ -5,17 +5,18 @@ from bibcite.work import Work
 
 def main():
     parser = argparse.ArgumentParser(description='Generate BibTeX citations from a title.')
-    parser.add_argument('title', type=str, help='The title of the work to generate a BibTeX citation for.')
+    parser.add_argument('--title', type=str, help='The title of the work to generate a BibTeX citation for')
+    parser.add_argument('--author', type=str, help='The author of the work', default=None)
     args = parser.parse_args()
 
     text = "Bibcite: Bibtex citations from the command line\n"
     table = [[text]]
     print(tabulate(table, tablefmt="grid"))
 
-    title = args.title
+    title, author = args.title, args.author
     try:
         print(f'- Searching for title \"{title}\"')
-        work = Work.from_query(title=title)
+        work = Work.from_query(title=title, author=author)
     except Exception as e:
         print(f'- An error occurred while trying to find work with title \"{title}\": {repr(e)}')
         return
